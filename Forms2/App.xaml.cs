8fd3fb940ffb0188f512088cb3c1a83f0e0ee103
jorge1208 <149629001+jorge1208@users.noreply.github.com>
2024-11-20 16:10:58 -0500

    Add files via upload

---
+++ Forms2/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Forms2.Services;
+using Forms2.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms2
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Forms2/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Forms2.Models;
+using Forms2.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms2.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
